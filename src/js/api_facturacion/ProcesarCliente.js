92089167d83f1bff74fa504809064cf8893e8f98
KenllyChacon <klchacon@uce.edu.ec>
2023-02-22 19:46:25 -0500

    taller_33

---
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//Metodos Fachada
+
+//Metodos de consumos CRUD
+const obtenerClientePorCedula = (cedula) => {
+
+}
+
+const insertar = (body) => {
+
+}
+
+const actualizar = (cedula, body) => {
+
+}
+
